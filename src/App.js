62800c96c6739611b4aac64a4a13794db126a7e4
Prajakta Bansod <prajaktab777@gmail.com>
2024-09-03 16:37:46 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./Header/Header";
+import Home from "./component/home/Home";
+import { Box } from "@mui/material";
+import DataProvider from "./context/dataProvider";
+import ProductDetail from "./component/details/ProductDetail";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CartProvider } from "./context/CartContext";
+import CartPages from "./component/cart/CartPages";
+ import EmtptyCart from "./component/cart/EmptyCart";
+ import CheckoutPage from "./component/cart/CheckoutPage";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  <BrowserRouter>
+        <DataProvider>
+          <CartProvider>
+            <Header />
+            <Box style={{ marginTop: 54 }}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/product/:id" element={<ProductDetail />} />  
+                <Route path="/cart" element={<CartPages />} /> 
+                 <Route path="/empty-cart" element={<EmtptyCart />} /> 
+                 <Route path="/checkout" element={<CheckoutPage />} />
+              </Routes>
+            </Box>
+          </CartProvider>
+        </DataProvider>
+      </BrowserRouter>
+    </>
   );
 }
 
